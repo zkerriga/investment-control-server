c1acf78025b5cc354ab4f8e14d07fd98d06c1f7f
zkerriga <daniil.63@yandex.ru>
2021-05-02 21:34:34 +0300

    Delete old implementation of routes

--- src/test/scala/ru/zkerriga/investment/routes/ServerApiSpec.scala
+++ src/test/scala/ru/zkerriga/investment/routes/ServerApiSpec.scala
@@ -3,11 +3,18 @@ package ru.zkerriga.investment.routes
 import akka.http.scaladsl.server.Route
 import monix.execution.Scheduler
 
+import ru.zkerriga.investment.api._
+
 
 class ServerApiSpec extends ServerApiSpecBase {
   implicit val s: Scheduler = monix.execution.Scheduler.global
 
   override def route: Route =
-    new TapirRoutes(mockServiceApi).routes
+    new ServerRoutesImpl(
+      List(
+        new MarketServerEndpoint(mockServiceApi, ExceptionHandlerForTask()),
+        new RegisterServerEndpoint(mockServiceApi, ExceptionHandlerForTask())
+      )
+    ).routes
 
 }
