a9f16d226c70923d1dafe4af42fbdc6c7b3aa76f
zkerriga <daniil.63@yandex.ru>
2021-05-11 17:41:46 +0300

    Add configuration

--- src/main/scala/ru/zkerriga/investment/Server.scala
+++ src/main/scala/ru/zkerriga/investment/Server.scala
@@ -3,26 +3,26 @@ package ru.zkerriga.investment
 import akka.Done
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
-import akka.http.scaladsl.model.Uri
 import akka.http.scaladsl.server.Route
 import monix.eval.Task
 import com.typesafe.scalalogging.LazyLogging
 
 import ru.zkerriga.investment.api.ServerRoutes
+import ru.zkerriga.investment.configuration.ServerConf
 
 
 case class Server(serverRoutes: ServerRoutes)(implicit as: ActorSystem) extends LazyLogging {
 
   private lazy val routes: Route = serverRoutes.routes
 
-  def start(url: Uri): Task[Http.ServerBinding] = Task.deferFutureAction { implicit scheduler =>
+  def start(config: ServerConf): Task[Http.ServerBinding] = Task.deferFutureAction { implicit scheduler =>
     Http()
-      .newServerAt(url.authority.host.address(), url.effectivePort)
+      .newServerAt(config.host, config.port.number)
       .bind(routes)
       .andThen { case _ =>
         logger.info(
-          s"""Server started at: $url
-             |See the documentation at: ${url.withPath(Uri.Path("/docs"))}""".stripMargin
+          s"""Server started at: ${ServerConf.getUri(config)}
+             |See the documentation at: ${ServerConf.getUri(config)}/docs""".stripMargin
         )
       }
     }
