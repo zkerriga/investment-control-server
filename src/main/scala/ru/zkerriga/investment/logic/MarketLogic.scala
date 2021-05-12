9debb7296be93e4e6e492ef85ee65873f6d7f804
zkerriga <daniil.63@yandex.ru>
2021-05-12 21:26:52 +0300

    Rewrite MarketLogic with EitherT

--- src/main/scala/ru/zkerriga/investment/logic/MarketLogic.scala
+++ src/main/scala/ru/zkerriga/investment/logic/MarketLogic.scala
@@ -1,9 +1,11 @@
 package ru.zkerriga.investment.logic
 
+import cats.data.EitherT
 import monix.eval.Task
 
 import ru.zkerriga.investment.entities.{StockOrder, VerifiedClient}
 import ru.zkerriga.investment.entities.openapi.{PlacedMarketOrder, Stocks}
+import ru.zkerriga.investment.exceptions.{DatabaseError, NotEnoughBalance, OpenApiResponseError, PageNotFound}
 
 
 trait MarketLogic {
@@ -14,13 +16,13 @@ trait MarketLogic {
    * @return a list of stocks that can be purchased on the exchange,
    *         and a PageNotFound if there is nothing on the page
    */
-  def getStocks(client: VerifiedClient, page: Int, onPage: Int): Task[Stocks]
+  def getStocks(client: VerifiedClient, page: Int, onPage: Int): EitherT[Task, Either[OpenApiResponseError, PageNotFound], Stocks]
 
   /**
    * Buys stocks using OpenAPI and registers asset tracking in the database
    * @return a response from OpenAPI with information about the request if success,
    *         and a NotEnoughBalance exception otherwise
    */
-  def buyStocks(client: VerifiedClient, stockOrder: StockOrder): Task[PlacedMarketOrder]
+  def buyStocks(client: VerifiedClient, stockOrder: StockOrder): EitherT[Task, Either[DatabaseError, NotEnoughBalance], PlacedMarketOrder]
 
 }
--- src/main/scala/ru/zkerriga/investment/logic/MarketLogicImpl.scala
+++ src/main/scala/ru/zkerriga/investment/logic/MarketLogicImpl.scala
@@ -1,8 +1,9 @@
 package ru.zkerriga.investment.logic
 
+import cats.data.EitherT
 import monix.eval.Task
 
-import ru.zkerriga.investment.{InternalError, NotEnoughBalance, PageNotFound}
+import ru.zkerriga.investment.exceptions.{DatabaseError, NotEnoughBalance, OpenApiResponseError, PageNotFound}
 import ru.zkerriga.investment.entities.{StockOrder, VerifiedClient}
 import ru.zkerriga.investment.entities.openapi.{MarketOrderRequest, PlacedMarketOrder, Stocks, TinkoffResponse}
 import ru.zkerriga.investment.storage.ClientDao
@@ -10,21 +11,30 @@ import ru.zkerriga.investment.storage.ClientDao
 
 class MarketLogicImpl(dao: ClientDao, openApiClient: OpenApiClient) extends MarketLogic {
 
-  override def getStocks(client: VerifiedClient, page: Int, onPage: Int): Task[Stocks] =
-    (openApiClient.`/market/stocks`(client.token) flatMapF {
-      case TinkoffResponse(_, _, stocks) =>
-        val result = stocks.instruments.slice(onPage * (page - 1), onPage * page)
+  def sliceStocks(stocks: Stocks, page: Int, onPage: Int): Either[PageNotFound, Stocks] = {
+    val sliced = stocks.instruments.slice(onPage * (page - 1), onPage * page)
+    Either.cond(sliced.isEmpty, Stocks(sliced.size, sliced), PageNotFound())
+  }
 
-        if (result.isEmpty)
-          Task.raiseError(PageNotFound())
-        else
-          Task.now(Right(Stocks(result.size, result)))
-    }).valueOrF(_ => Task.raiseError(InternalError("getStocks")))
+  override def getStocks(client: VerifiedClient, page: Int, onPage: Int): EitherT[Task, Either[OpenApiResponseError, PageNotFound], Stocks] =
+    openApiClient.`/market/stocks`(client.token)
+      .leftMap(Left.apply)
+      .flatMap {
+        case TinkoffResponse(_, _, stocks) =>
+          EitherT.fromEither(sliceStocks(stocks, page, onPage))
+      }
 
-  override def buyStocks(client: VerifiedClient, order: StockOrder): Task[PlacedMarketOrder] =
+  def registerStockInDb(id: Long, order: StockOrder): EitherT[Task, DatabaseError, Unit] =
+    dao.registerStock(id, order).map(_ => ())
+
+  override def buyStocks(client: VerifiedClient, order: StockOrder): EitherT[Task, Either[DatabaseError, NotEnoughBalance], PlacedMarketOrder] =
     openApiClient.`/orders/market-order`(client.token, order.figi, MarketOrderRequest(order.lots, "Buy"))
-      .map(_.payload)
-      .valueOrF(_ => Task.raiseError[PlacedMarketOrder](NotEnoughBalance())) <*
-        dao.registerStock(client.id, order)
+      .leftMap(_ => Right(NotEnoughBalance()))
+      .flatMap {
+        case TinkoffResponse(_, _, response) =>
+          registerStockInDb(client.id, order)
+            .leftMap(err => Left(err).withRight[NotEnoughBalance])
+            .map(_ => response)
+      }
 
 }
