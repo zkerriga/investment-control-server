dc0563bf040d03544ed2d21c1dc61a9c56e46d04
zkerriga <daniil.63@yandex.ru>
2021-05-12 20:56:30 +0300

    Rewrite VerifyLogic with EitherT

--- src/main/scala/ru/zkerriga/investment/logic/VerifyLogic.scala
+++ src/main/scala/ru/zkerriga/investment/logic/VerifyLogic.scala
@@ -1,27 +1,18 @@
 package ru.zkerriga.investment.logic
 
+import cats.data.EitherT
 import monix.eval.Task
 import sttp.tapir.model.UsernamePassword
 
 import ru.zkerriga.investment.entities.VerifiedClient
+import ru.zkerriga.investment.exceptions.{IncorrectCredentials, TokenDoesNotExist}
 import ru.zkerriga.investment.storage.entities.Client
 
 
 trait VerifyLogic {
 
-  /**
-   * Verifies the client's credentials.
-   *
-   * @return Information about the client from the database if successful,
-   *         and a IncorrectCredentials exception otherwise
-   */
-  def verifyCredentials(credentials: UsernamePassword): Task[Client]
+  def verifyCredentials(credentials: UsernamePassword): EitherT[Task, IncorrectCredentials, Client]
 
-  /**
-   * Checks the client token.
-   *
-   * @return the client if the token exists, and a TokenDoesNotExist exception otherwise
-   */
-  def verifyToken(client: Client): Task[VerifiedClient]
+  def verifyToken(client: Client): EitherT[Task, TokenDoesNotExist, VerifiedClient]
 
 }
--- src/main/scala/ru/zkerriga/investment/logic/VerifyLogicImpl.scala
+++ src/main/scala/ru/zkerriga/investment/logic/VerifyLogicImpl.scala
@@ -1,10 +1,10 @@
 package ru.zkerriga.investment.logic
 
-import cats.data.OptionT
+import cats.data.EitherT
 import monix.eval.Task
 import sttp.tapir.model.UsernamePassword
 
-import ru.zkerriga.investment.{IncorrectCredentials, TokenDoesNotExist}
+import ru.zkerriga.investment.exceptions.{DatabaseError, IncorrectCredentials, TokenDoesNotExist}
 import ru.zkerriga.investment.entities.{TinkoffToken, VerifiedClient}
 import ru.zkerriga.investment.storage.LoginDao
 import ru.zkerriga.investment.storage.entities.Client
@@ -12,20 +12,37 @@ import ru.zkerriga.investment.storage.entities.Client
 
 class VerifyLogicImpl(dao: LoginDao, bcrypt: AsyncBcrypt) extends VerifyLogic {
 
-  override def verifyCredentials(credentials: UsernamePassword): Task[Client] =
-    (for {
-      pass   <- OptionT.fromOption[Task](credentials.password)
-      client <- OptionT(dao.findClientByUsername(credentials.username))
-      result <- OptionT.liftF(bcrypt.verify(pass, client.passwordHash))
-      if result
-    } yield client)
-      .getOrElseF(Task.raiseError[Client](IncorrectCredentials()))
+  def findClientInDb(username: String): EitherT[Task, Either[DatabaseError, IncorrectCredentials], Client] =
+    dao.findClientByUsername(username)
+      .leftMap(Left.apply)
+      .flatMap { optClient =>
+        EitherT.fromOption(optClient, Right(IncorrectCredentials()))
+      }
 
-  override def verifyToken(client: Client): Task[VerifiedClient] =
-    (for {
-      id <- client.id
-      token <- client.token
-    } yield VerifiedClient(id, client.login, TinkoffToken(token)))
-      .fold(Task.raiseError[VerifiedClient](TokenDoesNotExist()))(Task.now)
+  def verityPassword(client: Client, password: String): EitherT[Task, IncorrectCredentials, Client] =
+    EitherT(
+      bcrypt.verify(password, client.passwordHash) map { check =>
+        Either.cond(check, client, IncorrectCredentials())
+      }
+    )
+
+  override def verifyCredentials(credentials: UsernamePassword): EitherT[Task, Either[DatabaseError, IncorrectCredentials], Client] =
+    credentials.password.fold(
+      EitherT.leftT[Task, Client](Right(IncorrectCredentials()).withLeft[DatabaseError])
+    ) { password =>
+      for {
+        client    <- findClientInDb(credentials.username)
+        verified  <- verityPassword(client, password).leftMap(err => Right(err).withLeft[DatabaseError])
+      } yield verified
+    }
+
+  override def verifyToken(client: Client): EitherT[Task, TokenDoesNotExist, VerifiedClient] =
+    EitherT.fromOption(
+      for {
+        id <- client.id
+        token <- client.token
+      } yield VerifiedClient(id, client.login, TinkoffToken(token)),
+      TokenDoesNotExist()
+    )
 
 }
